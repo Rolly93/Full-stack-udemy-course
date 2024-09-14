7cd1022e1b380383d62f02bc916e1c1d5d511a24
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2024-09-14 20:18:41 +0000

    se modifico el index principal para visualizar los ejercicios en visal studio code.
    vamos en el simmons game

---
+++ Udemy-Boot-Camp-FullStack/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,13 @@
+var bottonColours= ["red","blue","green","yellow"];
+var gamePattern = [];
+
+function nextSequence(){
+var randomNumber = Math.floor(Math.random()*4);
+var randomChoseColours = bottonColours[randomNumber];
+gamePattern.push(randomChoseColours);
+}
+$("button").on("click",function (click) {
+    $()    
+})
+
+
